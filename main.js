814a5acd5a93c634a401fd41c4a20e1d4fa52d28
Azamat <azamat3334817@gmail.com>
2022-12-27 14:30:08 +0500

    JavaScript projects

---
+++ main.js
@@ -0,0 +1,41 @@
+var name = prompt("Ismingizni kiriting");
+var age = prompt("Yoshingizni kiriting");
+alert("Sizning ismingiz " + name + " Siz " + age + " yoshdasiz");
+
+name = String(name);
+age = Number(age);
+
+var num = prompt("6 + 5 = ?");
+if (6 + 5 && !isNaN(4) && !isNaN(5)) {
+  alert(" Sizning javobingiz " + num + " togri javob " + (6 + 5) + " ga teng");
+} else {
+  console.log("Hatolik");
+}
+
+var num = prompt("6 - 5 = ?");
+if (6 - 5 && !isNaN(4) && !isNaN(5)) {
+  alert(" Sizning javobingiz " + num + " togri javob " + (6 - 5) + " ga teng");
+} else {
+  console.log("Hatolik");
+}
+
+var num = prompt("6 * 5 = ?");
+if (6 * 5 && !isNaN(4) && !isNaN(5)) {
+  alert(" Sizning javobingiz " + num + " togri javob " + (6 * 5) + " ga teng");
+} else {
+  console.log("Hatolik");
+}
+
+var num = prompt("2 / 4 = ?");
+if (6 / 5 && !isNaN(4) && !isNaN(5)) {
+  alert(" Sizning javobingiz " + num + " togri javob " + (2 / 4) + " ga teng");
+}else{
+    console.log("Hatolik");
+}
+
+var num = prompt("9 * 3 = ?");
+if (6 / 5 && !isNaN(4) && !isNaN(5)) {
+  alert(" Sizning javobingiz " + num + " togri javob " + (9 * 3) + " ga teng");
+}else{
+    console.log("Hatolik");
+}
